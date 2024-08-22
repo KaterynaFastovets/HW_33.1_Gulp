f2f57b0a471fb23e1aa9fd366e392230982e33a6
Kateryna Fastovets <katyafastovets@gmail.com>
2024-08-22 21:13:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const button = document.querySelector(".button-write-me ");
+let color = "blue";
+
+function change(button) {
+  button.style.background = color;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(10, 12));
